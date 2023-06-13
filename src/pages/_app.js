2134edbbe6bda624b3bd7ce0ff93002897b0d6b2
chyutronic <chyutronic@gmail.com>
2023-06-12 19:51:27 -0500

    Se agrega GTM (Google Tag Manager) al archivo _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,20 @@
 import "@/styles/globals.css";
 import "/public/fonts.css";
 import { DataContextProvider } from "@/contexts/DataContext";
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 
 export default function App({ Component, pageProps }) {
+  const gtmId = "GTM-MH7VL4R";
+
+  const tagManagerArgs = {
+    gtmId,
+  };
+
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return (
     <DataContextProvider>
       <Component {...pageProps} />
